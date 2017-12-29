9a2cb2333d5e091dc576df0b9ac21785b26b296a
kapilyadav1204 <kapilyadav1204@gmail.com>
2017-12-30 00:00:52 +0530

    second commit

--- TestGITFirst/src/test/java/TestGITFirst/TestGITFirst/AppTest.java
+++ TestGITFirst/src/test/java/TestGITFirst/TestGITFirst/AppTest.java
@@ -1,5 +1,7 @@
 package TestGITFirst.TestGITFirst;
 
+import org.testng.Assert;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,32 +9,17 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
+	@org.testng.annotations.Test
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	public void testinsideapp() {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+		System.out.println("Test 1");
+
+		for (int i = 0; i < 10; i++) {
+			System.out.println("i>>>>>>>>> " + i);
+
+			Assert.assertEquals(true, true);
+		}
+	}
 }
